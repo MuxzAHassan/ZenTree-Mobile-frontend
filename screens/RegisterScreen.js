88b3384cb0ce30f496d542fdf727dfd6bd53b3ee
Nik Ahmad Izzat <nikizzat0309@gmail.com>
2025-11-24 01:12:39 +0800

    Add Date of Birth Title

--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -1,215 +1,254 @@
-import React, { useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity, Pressable, KeyboardAvoidingView, StyleSheet, Image, Platform, ScrollView } from 'react-native';
-import  DateTimePicker from '@react-native-community/datetimepicker';
-import { ToggleButton } from 'react-native-paper';
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Pressable,
+  KeyboardAvoidingView,
+  StyleSheet,
+  Image,
+  Platform,
+  ScrollView,
+} from "react-native";
+import DateTimePicker from "@react-native-community/datetimepicker";
+import { ToggleButton } from "react-native-paper";
 
 export default function RegisterScreen({ navigation }) {
-
   const [form, setForm] = useState({
-    firstName: '',
-    lastName: '',
-    gender: '',
+    firstName: "",
+    lastName: "",
+    gender: "",
     dateOfBirth: new Date(),
-    phone: '',
-    email: '',
-    password: '',
+    phone: "",
+    email: "",
+    password: "",
   });
 
   const [showDatePicker, setShowDatePicker] = useState(false);
 
   const onChangeDate = (event, selectedDate) => {
     const currentDate = selectedDate || form.dateOfBirth;
-    setShowDatePicker(Platform.OS === 'ios');
-    setForm({...form, dateOfBirth: currentDate});
-  }
+    setShowDatePicker(Platform.OS === "ios");
+    setForm({ ...form, dateOfBirth: currentDate });
+  };
 
   const styles = StyleSheet.create({
     container: {
       flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      backgroundColor: '#fff',
+      justifyContent: "center",
+      alignItems: "center",
+      backgroundColor: "#fff",
       padding: 20,
     },
     title: {
       fontSize: 32,
-      fontWeight: 'bold',
+      fontWeight: "bold",
       marginBottom: 40,
-      color: '#B5651D',
+      color: "#B5651D",
     },
     input: {
-      width: '100%',
+      width: "100%",
       borderWidth: 1,
-      borderColor: '#ccc',
+      borderColor: "#ccc",
       borderRadius: 8,
       padding: 12,
       marginBottom: 15,
     },
     button: {
-      width: '100%',
-      backgroundColor: '#B5651D',
+      width: "100%",
+      backgroundColor: "#B5651D",
       padding: 15,
       borderRadius: 8,
-      alignItems: 'center',
+      alignItems: "center",
     },
     buttonText: {
-      color: '#fff',
+      color: "#fff",
       fontSize: 16,
-      fontWeight: 'bold',
+      fontWeight: "bold",
     },
     link: {
       marginTop: 15,
-      color: '#2E86C1',
-      alignSelf: 'flex-start',
+      color: "#2E86C1",
+      alignSelf: "flex-start",
     },
     label: {
-        fontSize: 16,
-        fontWeight: 'normal',
-        marginBottom: 8,
+      fontSize: 16,
+      fontWeight: "normal",
+      marginBottom: 8,
+      alignSelf: "flex-start",
     },
     toggle: {
-        borderWidth: 1,
-        borderRadius: 8,
-        borderColor: '#ccc',
-        marginBottom: 15,
-        paddingVertical: 10,
-        paddingHorizontal: 20,
+      borderWidth: 1,
+      borderRadius: 8,
+      borderColor: "#ccc",
+      marginBottom: 15,
+      paddingVertical: 10,
+      paddingHorizontal: 20,
     },
     activeToggle: {
-        backgroundColor: '#d3d3d3',
-        borderColor: '#d3d3d3',
+      backgroundColor: "#d3d3d3",
+      borderColor: "#d3d3d3",
     },
     text: {
-        fontSize: 14,
-        fontWeight: '500',
-        color: '#333',
+      fontSize: 14,
+      fontWeight: "500",
+      color: "#333",
     },
   });
 
   const handleRegister = async () => {
-
-    const { firstName, lastName, email, password, gender, phone} = form;
+    const { firstName, lastName, email, password, gender, phone } = form;
 
     //validation
-    if(!firstName || !lastName || !email || !phone || !form.dateOfBirth || !gender || !password) {
-      alert('Isi semua field la oi!');
+    if (
+      !firstName ||
+      !lastName ||
+      !email ||
+      !phone ||
+      !form.dateOfBirth ||
+      !gender ||
+      !password
+    ) {
+      alert("Isi semua field la oi!");
       return;
     }
 
-    try{
-      const response = await fetch('http://192.168.0.6:8080/api/auth/register', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json'},
-        body: JSON.stringify({
-          firstName,
-          lastName,
-          email,
-          password,
-          gender,
-          dateOfBirth: form.dateOfBirth,
-          phone
-        }),
-      });
+    try {
+      const response = await fetch(
+        "http://192.168.0.6:8080/api/auth/register",
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            firstName,
+            lastName,
+            email,
+            password,
+            gender,
+            dateOfBirth: form.dateOfBirth,
+            phone,
+          }),
+        }
+      );
 
       const data = await response.json();
 
-      if(response.ok) {
-        alert('Yey lepas register! Pi login lak');
-        navigation.navigate('Login');
-      } else{
-        alert(data.message || 'X lepas register');
+      if (response.ok) {
+        alert("Yey lepas register! Pi login lak");
+        navigation.navigate("Login");
+      } else {
+        alert(data.message || "X lepas register");
       }
-    } catch(error) {
-      alert('Error: ' + error.message);
+    } catch (error) {
+      alert("Error: " + error.message);
     }
-  }
+  };
 
   const gotoLogin = () => {
     // For now, just navigate to LoginScreen
-    navigation.navigate('Login');
+    navigation.navigate("Login");
   };
 
   return (
-    <KeyboardAvoidingView behavior='padding' style={{ flex: 1 }}>
-    <ScrollView contentContainerStyle={{ flexGrow: 1,}}>
-    <View style={styles.container}>
-        <Image source={require('../assets/NakUrut Logo only.png')} style={{width: 150, height: 150, marginBottom: 15}} resizeMode='contain'/>
-        <TextInput
-        style={styles.input}
-        placeholder="First Name"
-        value={form.firstName}
-        onChangeText={text => setForm({...form, firstName: text})}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Last Name"
-        value={form.lastName}
-        onChangeText={text => setForm({...form, lastName: text})}
-      />
+    <KeyboardAvoidingView behavior="padding" style={{ flex: 1 }}>
+      <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+        <View style={styles.container}>
+          <Image
+            source={require("../assets/NakUrut Logo only.png")}
+            style={{ width: 150, height: 150, marginBottom: 15 }}
+            resizeMode="contain"
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="First Name"
+            value={form.firstName}
+            onChangeText={(text) => setForm({ ...form, firstName: text })}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Last Name"
+            value={form.lastName}
+            onChangeText={(text) => setForm({ ...form, lastName: text })}
+          />
 
-      {/*Pressable function for gender selection*/}
-      <View style={{ width: '100%', alignItems: 'flex-start', marginBottom: 10 }}>
-        <Text style={styles.label}>Gender</Text>
-            <View style={{ flexDirection: 'row', gap: 10 }}>
-                <Pressable
-                    onPress={() => setForm({...form, gender: 'male'})} // set gender to 'male'
-                    style={[
-                        styles.toggle,
-                        form.gender === 'male' && styles.activeToggle
-                    ]}
-                >
-                    <Text style={styles.text}>Male</Text>
-                </Pressable>
+          {/*Pressable function for gender selection*/}
+          <View
+            style={{
+              width: "100%",
+              alignItems: "flex-start",
+              marginBottom: 10,
+            }}
+          >
+            <Text style={styles.label}>Gender</Text>
+            <View style={{ flexDirection: "row", gap: 10 }}>
+              <Pressable
+                onPress={() => setForm({ ...form, gender: "male" })} // set gender to 'male'
+                style={[
+                  styles.toggle,
+                  form.gender === "male" && styles.activeToggle,
+                ]}
+              >
+                <Text style={styles.text}>Male</Text>
+              </Pressable>
 
-                <Pressable
-                    onPress={() => setForm({...form, gender: 'female'})} // set gender to 'female'
-                    style={[
-                        styles.toggle,
-                        form.gender === 'female' && styles.activeToggle
-                    ]}
-                >
-                    <Text style={styles.text}>Female</Text>
-                </Pressable>
+              <Pressable
+                onPress={() => setForm({ ...form, gender: "female" })} // set gender to 'female'
+                style={[
+                  styles.toggle,
+                  form.gender === "female" && styles.activeToggle,
+                ]}
+              >
+                <Text style={styles.text}>Female</Text>
+              </Pressable>
             </View>
-        </View>
-        
-        {/*Date picker function for Date of Birth selection*/}    
-      <TouchableOpacity style={styles.input} onPress={() => setShowDatePicker(true)}>
-        <Text placeholder="Date of Birth">{form.dateOfBirth ? form.dateOfBirth.toDateString() : 'Please select your Date of Birth'}</Text>
-      </TouchableOpacity>
-        {showDatePicker && (
-        <DateTimePicker
-          value={form.dateOfBirth}
-          mode="date"
-          display="default"
-          onChange={onChangeDate}
-        />
-        )}
+          </View>
 
-      <TextInput
-        style={styles.input}
-        placeholder="Phone Number"
-        value={form.phone}
-        onChangeText={text => setForm({...form, phone: text})}
-        keyboardType="phone-pad"
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Email"
-        value={form.email}
-        onChangeText={text => setForm({...form, email: text})}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Password"
-        value={form.password}
-        onChangeText={text => setForm({...form, password: text})}
-      />
-      <TouchableOpacity style={styles.button} onPress={handleRegister}>
-        <Text style={styles.buttonText}>Register</Text>
-      </TouchableOpacity>
-    </View>
-    </ScrollView>
+          {/*Date picker function for Date of Birth selection*/}
+          <Text style={styles.label}>Date of Birth</Text>
+          <TouchableOpacity
+            style={styles.input}
+            onPress={() => setShowDatePicker(true)}
+          >
+            <Text placeholder="Date of Birth">
+              {form.dateOfBirth
+                ? form.dateOfBirth.toDateString()
+                : "Please select your Date of Birth"}
+            </Text>
+          </TouchableOpacity>
+          {showDatePicker && (
+            <DateTimePicker
+              value={form.dateOfBirth}
+              mode="date"
+              display="default"
+              onChange={onChangeDate}
+            />
+          )}
+
+          <TextInput
+            style={styles.input}
+            placeholder="Phone Number"
+            value={form.phone}
+            onChangeText={(text) => setForm({ ...form, phone: text })}
+            keyboardType="phone-pad"
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Email"
+            value={form.email}
+            onChangeText={(text) => setForm({ ...form, email: text })}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Password"
+            value={form.password}
+            onChangeText={(text) => setForm({ ...form, password: text })}
+          />
+          <TouchableOpacity style={styles.button} onPress={handleRegister}>
+            <Text style={styles.buttonText}>Register</Text>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
     </KeyboardAvoidingView>
   );
 }
